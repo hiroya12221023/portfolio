53458f879e4057b1f03fbbc0a8392a47d4229612
hiorya12221023 <a1211065@kuins.ac.jp>
2023-03-07 18:26:37 +0900

    適切なコミットコメント

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,10 +7,18 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(Post $post)
-    {
-        return view('posts/index')->with(['posts' => $post->get()]);  
-       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
-    }
+ public function index(Post $post)
+{
+    return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
 }
-?>
+public function show(Post $post)
+{
+    return view('posts/show')->with(['post' => $post]);
+ //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+}
+public function create()
+{
+    return view('posts/create');
+}
+}
+
